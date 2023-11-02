72c9b16f57b9fdb828b977fca9c44a9c4215705d
alvarocastellano <alvcasalo@alum.us.es>
2023-11-02 21:02:23 +0100

    Add: Practica 1 y 2

--- decide/authentication/tests.py
+++ decide/authentication/tests.py
@@ -6,7 +6,14 @@ from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 
 from base import mods
+from django.test import TestCase
 
+class SimpleTest(TestCase):
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
 
 class AuthTestCase(APITestCase):
 
